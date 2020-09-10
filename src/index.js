4f3d4709c4c5c8b0c8a838ef469327372348f6f5
Jack Reed <phillipjreed@gmail.com>
2020-09-10 15:26:56 -0600

    Setup annotations

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import Mirador from 'mirador/dist/es/src/index';
 import { miradorImageToolsPlugin } from 'mirador-image-tools';
 import MiradorDownload from 'mirador-dl-plugin';
 import MiradorDownloadDialogPlugin from 'mirador-dl-plugin';
-
+import miradorAnnotationPlugins from 'mirador-annotations/es/index';
+import LocalStorageAdapter from 'mirador-annotations/es/LocalStorageAdapter';
 
 const config = {
+  annotation: {
+    adapter: (canvasId) => new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`),
+  },
   id: 'demo',
+  window: {
+    defaultSideBarPanel: 'annotations',
+  },
   windows: [{
     imageToolsEnabled: true,
     manifestId: 'https://purl.stanford.edu/sn904cj3429/iiif/manifest',
@@ -23,4 +30,5 @@ Mirador.viewer(config, [
   ...miradorImageToolsPlugin,
   MiradorDownload.miradorDownloadPlugin,
   MiradorDownload.MiradorDownloadDialogPlugin,
+  ...miradorAnnotationPlugins,
 ]);
