b2d59bd98f13da93cd8bcabebe3e00909ba024c0
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2024-02-15 16:39:17 +0100

    Mirador and mirator annotations editor plugin running smoothly

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,101 @@
 const path = require('path');
+const fs = require('fs');
+
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        include: path.resolve(fs.realpathSync(process.cwd()), '.'), // CRL
+        loader: require.resolve('babel-loader'),
+        options: {
+          // Save disk space when time isn't as important
+          cacheCompression: true,
+          cacheDirectory: true,
+          compact: true,
+          envName: 'development',
+          presets: [
+            ['@babel/preset-env',
+            {
+              corejs: 3,
+              exclude: ['transform-typeof-symbol'],
+              forceAllTransforms: true,
+              modules: false,
+              useBuiltIns: 'entry',
+            }],
+            [
+              '@babel/preset-react',
+              {
+                development: true,
+                runtime: 'automatic',
+                useBuiltIns: true,
+              },
+            ],
+          ],
+          plugins:[
+            'babel-plugin-macros',
+            '@babel/plugin-transform-destructuring',
+            [
+              '@babel/plugin-proposal-class-properties',
+              {
+                loose: true,
+              },
+            ],
+            ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+            [
+              '@babel/plugin-proposal-object-rest-spread',
+              {
+                useBuiltIns: true,
+              },
+            ],
+            [
+              '@babel/plugin-transform-runtime',
+              {
+                corejs: false,
+                helpers: false, // Needed to support IE/Edge
+                regenerator: true,
+              },
+            ],
+            [
+              '@babel/plugin-transform-regenerator',
+              {
+                async: false,
+              },
+            ],
+            ['transform-react-remove-prop-types',
+              {
+                ignoreFilenames: ['node_modules'],
+                removeImport: true,
+              },
+            ],
+            ['lodash', {
+              id: [
+                'lodash',
+              ],
+            },
+            ],
+          ],
+        },
+        test: /\.(js|mjs|jsx)$/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: './dist/',
+  },
+  mode: 'development',
+  resolve: {
+    alias: {
+      'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
+      'react/jsx-runtime': 'react/jsx-runtime.js',
+    },
+    extensions: ['.js'],
   },
 };
